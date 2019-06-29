c74f2eca13fd1d0145568db0e9edd07f7ed302de
Buster Charity <bcharity@il-g4.21-school.ru>
2019-06-29 15:29:03 +0300

    Kekech

---
+++ includes copy/fillit.h
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fillit.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sskinner <sskinner@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/31 17:25:58 by sskinner          #+#    #+#             */
+/*   Updated: 2019/06/27 18:57:11 by bcharity         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FILLIT_H
+# define FILLIT_H
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include "../libft/libft.h"
+# include <stdio.h>
+typedef struct s_figure	t_fig;
+
+struct		s_figure
+{
+	int		*x_arr;
+	int		*y_arr;
+	int		index;
+	t_fig	*next;
+};
+char		**create_map (t_fig *list, int a);
+char		**zero_map(int a);
+int			fill_map(int a, t_fig *fig, char **map);
+int			check_avail(t_fig *list, int dx, int dy, char **map);
+void		to_map(t_fig *list, int dx, int dy, char **map);
+char		*free_map(char **map);
+void		print_map(char **map);
+char		*reading(int ac, char *av);
+t_fig		*tetri_new();
+t_fig		*new_list(t_fig **head, int l);
+void		tetri_del(t_fig **base);
+void		tetri_absolute(t_fig **head);
+int			validate_1(char *str);
+int			validate_2(char *str);
+t_fig		*validate_4(int *st, int l);
+t_fig		*validate_3(char *str, int l);
+t_fig		*validate_sub(char *buf, int l);
+t_fig		*validate_main(char *str, int count);
+t_fig		*create_list(int *st, t_fig **head, int l);
+int         func_flag(const int vis[8], const int *stack);
+int			getvid(int *stack, int i);
+int			min_edge(t_fig *list);
+void		clear_tetr(t_fig *list, int dx, int dy, char **map);
+#endif
---
+++ src copy/fillit_funk_2.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fillit_funk_2.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sskinner <sskinner@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/31 16:41:20 by sskinner          #+#    #+#             */
+/*   Updated: 2019/06/26 21:00:18 by sskinner         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/fillit.h"
+
+t_fig	*create_list(int *st, t_fig **head, int l)
+{
+	int				i;
+	static t_fig	*list;
+
+	i = 0;
+	if(!(list = new_list(head, l)))
+        tetri_del(head);
+	while (i < 4)
+	{
+		list->x_arr[i] = st[i];
+		list->y_arr[i] = st[i + 4];
+		i++;
+	}
+	return (*head);
+}
+
+t_fig	*new_list(t_fig **head, int l)
+{
+	t_fig	*tmp;
+	t_fig	*new;
+	int		i;
+
+	i = 0;
+	tmp = *head;
+	if (l == 1)
+	{
+		(*head)->index = 0;
+		return (*head);
+	}
+	new = tetri_new();
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+	new->index = l - 1;
+	tmp->next = new;
+	return (new);
+}
+
+char	*reading(int ac, char *av)
+{
+	char	str[546];
+	char	*tmp;
+	int		count;
+	int		fd;
+
+	if (ac != 2)
+	{
+		ft_putendl("usage [map]");
+		return (NULL);
+	}
+	tmp = NULL;
+	if ((fd = open(av, O_RDONLY)) == -1)
+		return (NULL);
+	count = read(fd, str, 546);
+	str[count] = '\0';
+	tmp = ft_strndup(str, count);
+	if ((count = read(fd, str, 546)) > 0)
+		return (NULL);
+	close(fd);
+	return (tmp);
+}
